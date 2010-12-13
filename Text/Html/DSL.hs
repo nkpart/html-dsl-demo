89ff169c4e66efcfc22a176f151b1c9ba651f75a
Nick Partridge <nkpart@gmail.com>
2010-12-13 15:22:24 +1000

    proper monad wrapper

--- Text/Html/DSL.hs
+++ Text/Html/DSL.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Text.Html.DSL (
   -- Tag creation methods
+  Tags,
   tag,
   empty,
   text,
@@ -12,17 +14,18 @@ module Text.Html.DSL (
 import Control.Monad.Writer
 
 -- | This is the monad that lets us combine things that write out stuff
-type Tags = Writer String () 
+newtype TagsM a = TagsM (Writer String a) deriving (Monad)
+type Tags = TagsM ()
 
 -- | Dumps the contents of written tags to a string
 dumpTags :: Tags -> String
-dumpTags t = snd . runWriter $ t -- When you run a writer, you get a pair of (value, writerState).
+dumpTags (TagsM t) = snd . runWriter $ t -- When you run a writer, you get a pair of (value, writerState).
                                  -- We don't care about the value, and just want the tags that were
                                  -- written to the state.
 
 -- | Create a tag from a name and children
 tag :: String -> Tags -> Tags
-tag tagName children = do
+tag tagName (TagsM children) = TagsM $ do
   -- This uses special methods written for the "Writer" monad, `tell` adds
   -- output to the backing state
   tell $ "<" ++ tagName ++ ">"
@@ -35,13 +38,13 @@ empty = return () -- we've not 'tell'ed anything so nothing gets written
 
 -- | A tag that writes text
 text :: String -> Tags
-text = tell -- writing text is just a straight tell
+text = TagsM . tell -- writing text is just a straight tell
 
 -- | The example
+test1 :: Tags
 test1 = tag "html" $ do
   tag "p" $ do
     tag "strong" $ text "Hi guys" 
   tag "div" $ text "more things"
-    
 
 
